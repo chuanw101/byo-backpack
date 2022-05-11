02de10b9d16911ca6e1b3437eabec21a4d420e6c
Chuan Wang <chuan.wang101@gmail.com>
2022-05-11 16:30:08 -0700

    stop scrolling when add/remove item

--- public/js/updateEvent.js
+++ public/js/updateEvent.js
@@ -1,4 +1,4 @@
-let public = ( document.querySelector("#public").getAttribute("checked") == "checked" );
+let public = (document.querySelector("#public").getAttribute("checked") == "checked");
 var photoArr = []
 console.log(document.querySelector('#startTime').getAttribute("temp"))
 
@@ -7,88 +7,88 @@ for (const radioButton of radioButtons) {
     radioButton.addEventListener('click', radioBtnEvent);
 
     function radioBtnEvent(e) {
-        
+
 
         if (e.target.value == 1) {
-            
+
 
             public = true;
-            
+
         } else {
             public = false;
-            
+
         }
     }
 }
 
 const myWidget = cloudinary.createUploadWidget(
     {
-      cloudName: 'da2jrzaai',
-      uploadPreset: 'usrvqzja',
-      maxImageFileSize: 2000000,  //restrict file size to less than 2MB
-      multiple: false,
-      theme: "minimal"
+        cloudName: 'da2jrzaai',
+        uploadPreset: 'usrvqzja',
+        maxImageFileSize: 2000000,  //restrict file size to less than 2MB
+        multiple: false,
+        theme: "minimal"
     },
     (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log("Done! Here is the image info: ", result.info);
-        document
-          .getElementById("uploadedimage")
-          .setAttribute("src", result.info.secure_url);
-        photoArr.push(result.info)
-      }
+        if (!error && result && result.event === "success") {
+            console.log("Done! Here is the image info: ", result.info);
+            document
+                .getElementById("uploadedimage")
+                .setAttribute("src", result.info.secure_url);
+            photoArr.push(result.info)
+        }
     }
-  );
-  
-  document.getElementById("upload_widget").addEventListener(
+);
+
+document.getElementById("upload_widget").addEventListener(
     "click", e => {
-      e.preventDefault()
-      myWidget.open();
+        e.preventDefault()
+        myWidget.open();
     },
     false
-  );
+);
 
 
 // update Event
 const updateEventHandler = async (event) => {
-    
+
     try {
 
-    event.preventDefault();
-    const event_name = document.querySelector('#eventName').value;
-    const location = document.querySelector('#location').value;
-    const start_time = document.querySelector('#startTime').value;
-    const end_time = document.querySelector('#endTime').value;
-    const picture_path = document.querySelector('#uploadedimage').src;
-    // const items = document.querySelector('#backpackItems').value;
-    const city = document.querySelector("#city").value
-    const state = document.querySelector("#state").value
-   
-    const description = document.querySelector('#eventDescription').value;
+        event.preventDefault();
+        const event_name = document.querySelector('#eventName').value;
+        const location = document.querySelector('#location').value;
+        const start_time = document.querySelector('#startTime').value;
+        const end_time = document.querySelector('#endTime').value;
+        const picture_path = document.querySelector('#uploadedimage').src;
+        // const items = document.querySelector('#backpackItems').value;
+        const city = document.querySelector("#city").value
+        const state = document.querySelector("#state").value
 
-    const eventId = document.querySelector('#updateEventSubmit').value;
-    
-    const response = await fetch(`/api/events/${eventId}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            event_name,
-            location,
-            city,
-            state,
-            // start_time,
-            // end_time,
-            picture_path,
-            description,
-            public,
-            // items: req.body.items,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    
-    alert('the Event has been updated')
-   document.location.replace('/profile');
+        const description = document.querySelector('#eventDescription').value;
+
+        const eventId = document.querySelector('#updateEventSubmit').value;
+
+        const response = await fetch(`/api/events/${eventId}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                event_name,
+                location,
+                city,
+                state,
+                // start_time,
+                // end_time,
+                picture_path,
+                description,
+                public,
+                // items: req.body.items,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        alert('the Event has been updated')
+        document.location.replace('/profile');
 
 
 
@@ -104,7 +104,7 @@ document.querySelector('#updateEventSubmit').addEventListener('click', updateEve
 // delete Item
 const deleteItems = document.querySelectorAll(".deleteItem");
 for (const deleteItem of deleteItems) {
-    
+
     deleteItem.addEventListener('click', deleteEvent);
 
     async function deleteEvent(e) {
@@ -116,7 +116,7 @@ for (const deleteItem of deleteItems) {
             }
         });
         if (response.ok) {
-            document.location.replace(`/profile/update/${e.target.getAttribute("name")}`);
+            document.location.reload(true);
         } else {
             alert('Failed to delete');
         }
@@ -126,12 +126,11 @@ for (const deleteItem of deleteItems) {
 // Add new Item
 const newItemHandler = async (event) => {
     try {
-
-    event.preventDefault();
-    const item_name = document.querySelector('#newItem').value.trim();
-    if(item_name){
-        const eventId = document.querySelector(".newItemBtn").getAttribute("name");
-        const owner_id = document.querySelector(".newItemBtn").value
+        event.preventDefault();
+        const item_name = document.querySelector('#newItem').value.trim();
+        if (item_name) {
+            const eventId = document.querySelector(".newItemBtn").getAttribute("name");
+            const owner_id = document.querySelector(".newItemBtn").value
             const response = await fetch(`/api/items/${eventId}`, {
                 method: 'POST',
                 body: JSON.stringify({
@@ -142,18 +141,15 @@ const newItemHandler = async (event) => {
                     'Content-Type': 'application/json',
                 },
             });
-            document.location.replace(`/profile/update/${eventId}`);
-        
-    }else{
-        alert("please enter Item name")
-    }
-    
+            document.location.reload(true);
 
+        } else {
+            alert("please enter Item name")
+        }
 
     } catch (err) {
         alert('the event didnt updated')
         console.log(err)
-
     }
 };
 document.querySelector('.newItemBtn').addEventListener('click', newItemHandler);
